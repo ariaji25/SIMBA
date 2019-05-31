0b57b69cce3e47bd1b703f47628d1b7ed6e558ef
ariaji25 <ari.purnama838@gmail.com>
2019-05-31 19:08:58 +0800

    date picker

--- app/src/main/java/com/example/simba/AddFirstAsset.java
+++ app/src/main/java/com/example/simba/AddFirstAsset.java
@@ -1,18 +1,27 @@
 package com.example.simba;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
+import android.icu.util.Calendar;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
 
 public class AddFirstAsset extends AppCompatActivity {
     private Button addAsset;
+    private EditText tanggal;
+    private DatePickerDialog datePickerDialog;
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_first_asset);
-
+        tanggal = findViewById(R.id.tanggal);
         addAsset = findViewById(R.id.tambah);
         addAsset.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -20,6 +29,31 @@ public class AddFirstAsset extends AppCompatActivity {
                 goHome();
             }
         });
+
+        tanggal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Calendar c = Calendar.getInstance();
+                int mYear = c.get(Calendar.YEAR); // current year
+                int mMonth = c.get(Calendar.MONTH); // current month
+                int mDay = c.get(Calendar.DAY_OF_MONTH); // current day
+                // date picker dialog
+                datePickerDialog = new DatePickerDialog(AddFirstAsset.this,
+                        new DatePickerDialog.OnDateSetListener() {
+
+                            @Override
+                            public void onDateSet(DatePicker view, int year,
+                                                  int monthOfYear, int dayOfMonth) {
+                                // set day of month , month and year value in the edit text
+                                tanggal.setText(dayOfMonth + "/"
+                                        + (monthOfYear + 1) + "/" + year);
+
+                            }
+                        }, mYear, mMonth, mDay);
+                datePickerDialog.show();
+            }
+        });
+
     }
 
     void goHome(){
